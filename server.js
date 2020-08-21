924fb22ee1563c74b548dcc1326f74fe5d77c1dc
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-21 13:47:33 -0700

    added table button

--- server.js
+++ server.js
@@ -3,3 +3,29 @@ var path = require("path");
 
 var app = express();
 var PORT = 3000;
+
+// Routes
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
